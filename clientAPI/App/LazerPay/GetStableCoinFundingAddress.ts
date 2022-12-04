d9f72c88e8447e1f2c78c2d50189143d16866b08
Oluwatobi Akanji <akanjioluwatobishadrach@yahoo.com>
2022-12-04 21:30:03 +0100

    chore: updated funding address

--- clientAPI/App/LazerPay/GetStableCoinFundingAddress.ts
+++ clientAPI/App/LazerPay/GetStableCoinFundingAddress.ts
@@ -5,7 +5,7 @@ import { getAPIBaseURL } from 'Environment';
 import { getAuthHeader } from '../Auth';
 
 export const GetStableCoinFundingAddress = async (coin: Coins) => {
-	const route = '/tokens/address';
+	const route = '/lazerpay/tokens/address';
 
 	return await API.GET<any, {address: string}>(
 		getAPIBaseURL(), route, { coin }, {
